ae15a701b9b39b7e4aaf1bdab0fd45f29acb3d63
Pedroenrick <pedro_penna22@hotmail.com>
2022-03-01 23:21:38 -0300

    modificando pagina para Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from "next";
+import { GetStaticProps } from "next";
 import styles from "./home.module.scss";
 import Head from "next/head";
 import { SubscribeButton } from "../components/SubscribeButton";
@@ -34,7 +34,7 @@ export default function Home({ product }: IHomeProps) {
   );
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve("price_1KYhAlEQffHHGpXxaF24ORgD");
 
   const product = {
@@ -49,5 +49,6 @@ export const getServerSideProps: GetServerSideProps = async () => {
     props: {
       product,
     },
+    revalidate: 60 * 60 * 24, //24 hours
   };
 };
