c17ed4af7d3387acb745f19d7a92ddcfdf0420d9
Pedroenrick <pedro_penna22@hotmail.com>
2022-03-07 21:26:32 -0300

    [FEATURE] adicionando faunaDB

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,17 @@
-import NextAuth from "next-auth";
+import NextAuth, { Account, Profile, User } from "next-auth";
+import { query } from "faunadb";
 import GithubProvider from "next-auth/providers/github";
+
+import { fauna } from "../../../services/fauna";
+
+interface signInProps {
+  user: User;
+  account: Account;
+  profile: Profile;
+  email: {
+    verificationRequest?: boolean;
+  };
+}
 
 export default NextAuth({
   providers: [
@@ -8,4 +20,37 @@ export default NextAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+
+  callbacks: {
+    async signIn({ user, account, profile }: signInProps) {
+      const { email } = user;
+
+      try {
+        await fauna.query(
+          query.If(
+            query.Not(
+              query.Exists(
+                query.Match(
+                  query.Index("user_by_email"),
+                  query.Casefold(user.email)
+                )
+              )
+            ),
+            query.Create(query.Collection("user"), { data: { email } }),
+            query.Get(
+              query.Match(
+                query.Index("user_by_email"),
+                query.Casefold(user.email)
+              )
+            )
+          )
+        );
+
+        return true;
+      } catch (err) {
+        console.error(err);
+        return false;
+      }
+    },
+  },
 });
